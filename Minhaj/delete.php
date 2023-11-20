304e63d7d2d37a53522f1385609f2cc7550f7174
muhpuc40 <mdhassan49.muh@gmail.com>
2023-11-21 00:36:10 +0600

    update minhaj

--- Minhaj/delete.php
+++
@@ -1,8 +0,0 @@
-<?php include 'connection.php'; ?>
-<?php 
-    $teacher_id = $_REQUEST['tId'];
-    $str = "DELETE from department_admins WHERE id=$teacher_id";
-    if(mysqli_query($conn, $str)){
-        header('Location: dashboard.php');
-    }
-?>
