16da15e98c5ef7aca6d6d655fe607b34229db4ee
Tonmoy Barua <46046251+Tonmoyhridhaan@users.noreply.github.com>
2023-11-21 00:52:35 +0600

    Authentication addedf

---
+++ Tonmoy/index.php
@@ -0,0 +1,83 @@
+<?php
+ session_start();
+ if(isset($_SESSION['user'])){
+    if($_SESSION['user']=='admin'){
+        header('Location: admin/dashboard.php');
+    }
+    else if($_SESSION['user']=='employee'){
+        header('Location: employee/dashboard.php');
+    
+ }
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+  <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+</head>
+<body>
+ 
+<div class="container">
+  <h2>Login Page</h2>
+  <p>Enter Your email and password to login</p>
+  <div class="card" style="width:500px">
+    <form method="post" class="pl-5 pr-5 pb-5 pt-5">
+        <div class="form-group">
+            <label for="email">Email:</label>
+            <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
+        </div>
+        <div class="form-group">
+            <label for="pwd">Password:</label>
+            <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pswd">
+        </div>
+        
+        <button type="submit" name="submit" value="submit" class="btn btn-primary">Login</button>
+    </form>
+    <?php
+        include 'connection.php';
+        if($_POST['submit']){
+            $email = $_POST['email'];
+            $pswd = $_POST['pswd'];
+            
+            $query = "SELECT * FROM admins WHERE email='$email' AND password='$pswd' ";
+            $result = mysqli_query($con,$query);
+            $admin = mysqli_fetch_array($result);
+
+            $query = "SELECT * FROM employees WHERE email='$email' AND password='$pswd' ";
+            $result = mysqli_query($con,$query);
+            $employee = mysqli_fetch_array($result);
+
+            if($admin){
+                $_SESSION['user'] = 'admin';
+                $_SESSION['id'] = $admin['id'];
+                header("Location: admin/dashboard.php");
+            }
+            else if($employee){
+                $_SESSION['user'] = 'employee';
+                $_SESSION['id'] = $employee['id'];
+                header("Location: employee/dashboard.php");
+            }
+            else{
+                echo "<span class='text-center text-danger' >Invalid Email or Password!</span>";
+            }
+
+
+        }
+
+    ?>
+  </div>
+  <br>
+  
+  
+  
+</div>
+
+</body>
+</html>
