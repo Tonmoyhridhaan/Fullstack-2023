8c211cd76602cf5adb0683db1c6067e2a3d09b3b
muhpuc40 <mdhassan49.muh@gmail.com>
2023-11-30 01:07:27 +0600

    hw

--- Minhaj/connection.php
+++ Minhaj/connection.php
@@ -9,6 +9,6 @@ $conn = mysqli_connect($hostname, $username, $password, $db);
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-// echo "Connected successfully";
+//echo "Connected successfully";
 
 ?>
