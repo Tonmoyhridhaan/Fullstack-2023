e1cbf3ba337ab1c78e28182bc1e1c4a5d2c61881
muhpuc40 <mdhassan49.muh@gmail.com>
2023-11-30 23:04:00 +0600

    layouting error

--- Minhaj/admin/employees.php
+++ Minhaj/admin/employees.php
@@ -5,29 +5,19 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+
 </head>
 <body>
 <div class="container">
   <h2>Employees Table</h2>
-  <p>Full stack employees</p>  
-  <table class="inputs">
-        <tbody><tr>
-            <td>Minimum age:</td>
-            <td><input type="text" id="min" name="min"></td>
-        </tr>
-        <tr>
-            <td>Maximum age:</td>
-            <td><input type="text" id="max" name="max"></td>
-        </tr>
-    </tbody>
-  </table>          
+  <p>Full stack employees</p>           
   <table class="display" style="width:100%" id="example">
     <thead>
       <tr>
         <th>Name</th>
         <th>Email</th>
         <th>Age</th>
-        <th>Contact</th>
+        <th>Salary</th>
         <th>Location</th>
         <th>Action</th>
       </tr>
@@ -43,7 +33,7 @@
         <td><?php echo $row['name']; ?></td>
         <td><?php echo $row['email']; ?></td>
         <td><?php echo $row['age']; ?></td>
-        <td><?php echo $row['contact']; ?></td>
+        <td><?php echo $row['salary']; ?></td>
         <td><?php echo $row['location']; ?></td>
         <td>
             <button class="btn btn-warning">Edit</button>
@@ -54,7 +44,47 @@
       <?php } ?>
       
     </tbody>
+    <tfoot>
+            <tr>
+                <th colspan="3" style="text-align:right">Total:</th>
+                <th></th>
+            </tr>
+        </tfoot>
   </table>
 </div>
+<script src="https://code.jquery.com/jquery-3.7.0.js"></script>
+<script src="https://cdn.datatables.net/1.13.7/js/jquery.dataTables.min.js"></script>
+<script>
+new DataTable('#example', {
+    footerCallback: function (row, data, start, end, display) {
+        let api = this.api();
+ 
+        // Remove the formatting to get integer data for summation
+        let intVal = function (i) {
+            return typeof i === 'string'
+                ? i.replace(/[\$,]/g, '') * 1
+                : typeof i === 'number'
+                ? i
+                : 0;
+        };
+ 
+        // Total over all pages
+        total = api
+            .column(3)
+            .data()
+            .reduce((a, b) => intVal(a) + intVal(b), 0);
+ 
+        // Total over this page
+        pageTotal = api
+            .column(3, { page: 'current' })
+            .data()
+            .reduce((a, b) => intVal(a) + intVal(b), 0);
+ 
+        // Update footer
+        api.column(3).footer().innerHTML =
+            '$' + pageTotal + ' ( $' + total + ' total)';
+    }
+});
+</script>
 </body>
 </html>
--- Minhaj/employee/dashboard.php
+++ Minhaj/employee/dashboard.php
@@ -1,12 +1,64 @@
+<?php
+    session_start();
+
+    //authentication
+    if(!isset($_SESSION['user'])){
+        header('Location: ../logout.php');
+    }
+    //Authorization
+   if($_SESSION['user'] != 'employee'){
+      header('Location: ../unauthorized.php');
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.7.1/dist/jquery.slim.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="style.css">
+
     <title>Document</title>
 </head>
-<body>
-    <h2>This is employee dashboard</h2>
-    <a href="../logout.php"> Logout
-</body>
+    <body>
+
+        <?php include 'navbar.php'; ?>
+
+        <div id="wrapper" class="toggled">
+            <div class="container-fluid">
+                <!-- Sidebar -->
+
+                <?php include 'sidebar.php'; ?>
+
+                <!-- /#sidebar-wrapper -->
+
+                <!-- Page Content -->
+                <div id="page-content-wrapper">
+                    <div class="container-fluid">
+                        <div class="row">
+                            <div class="col-lg-12">
+                            <br>
+                            <h1>Your Title</h1>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-12">
+                                Your Content
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- /#page-content-wrapper -->
+            </div>
+        </div>
+        <!-- /#wrapper -->
+            <h2>This is employee dashboard</h2>
+
+        <script src="main.js"></script>
+
+    </body>
 </html>
